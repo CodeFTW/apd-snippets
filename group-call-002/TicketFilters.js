4e872fad71f9c868310fd906e615d5a7fe40e4eb
filipenevola <filipe@quave.com.br>
2024-04-13 12:19:34 -0400

    group-call-002 - test if lefthook will use prettier to format my code

--- group-call-002/TicketFilters.js
+++ group-call-002/TicketFilters.js
@@ -1,37 +1,37 @@
 export const TicketFilters = {
-    BY_NAME: {
-        propName: 'eventName',
-        isRequired: false,
-        execute({query: {eventName}, event}) {
-            return !eventName || event.eventName.includes(eventName);
-        },
+  BY_NAME: {
+    propName: "eventName",
+    isRequired: false,
+    execute({ query: { eventName }, event }) {
+      return !eventName || event.eventName.includes(eventName);
     },
-    BY_DATE: {
-        propName: 'eventDate',
-        isRequired: false,
-        execute({query: {eventDate}, event}) {
-            return !eventDate || event.eventDate === eventDate;
-        },
+  },
+  BY_DATE: {
+    propName: "eventDate",
+    isRequired: false,
+    execute({ query: { eventDate }, event }) {
+      return !eventDate || event.eventDate === eventDate;
     },
-    BY_QUANTITY: {
-        propName: 'quantity',
-        isRequired: true,
-        execute({query: {quantity}, event}) {
-            return !quantity || event.quantity >= quantity;
-        },
+  },
+  BY_QUANTITY: {
+    propName: "quantity",
+    isRequired: true,
+    execute({ query: { quantity }, event }) {
+      return !quantity || event.quantity >= quantity;
     },
-    BY_STATUS: {
-        propName: 'status',
-        isRequired: false,
-        execute({query: {status}, event}) {
-            return !status || event.eventStatus === status;
-        },
+  },
+  BY_STATUS: {
+    propName: "status",
+    isRequired: false,
+    execute({ query: { status }, event }) {
+      return !status || event.eventStatus === status;
     },
-    BY_USER_VIP: {
-        propName: 'quantity',
-        isRequired: false,
-        execute({event, user}) {
-            return user?.isVIP || !event.onlyVIPUsers;
-        },
+  },
+  BY_USER_VIP: {
+    propName: "quantity",
+    isRequired: false,
+    execute({ event, user }) {
+      return user?.isVIP || !event.onlyVIPUsers;
     },
+  },
 };
